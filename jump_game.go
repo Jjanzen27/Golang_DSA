45e2cd8ffb25429b775157f437bbe5f647b6185e
Jenae Janzen <jenaejanzen@gmail.com>
2021-09-07 14:40:14 +0000

    dp

--- jump_game.go
+++ jump_game.go
@@ -38,26 +38,98 @@ import (
 // 	return true
 // }
 
-func canJump(nums []int) bool {
-	availSteps := nums[0]
-	return helper(nums, availSteps)
-}
+// func canJump(nums []int) bool {
+// 	availSteps := nums[0]
+// 	return helper(nums, availSteps)
+// }
 
-func helper(nums []int, availSteps int) bool {
-	//fmt.Println(nums, availSteps)
-	if len(nums) == 1 {
+// func helper(nums []int, availSteps int) bool {
+// 	//fmt.Println(nums, availSteps)
+// 	if len(nums) == 1 {
+// 		return true
+// 	}
+
+// 	if nums[0] > availSteps {
+// 		availSteps = nums[0]
+// 	}
+
+// 	if availSteps == 0 {
+// 		return false
+// 	}
+
+// 	return helper(nums[1:], availSteps-1)
+// }
+
+// Yujohn's solution
+
+// func canJump(nums []int) bool {
+// 	return helper(nums, len(nums)-2, len(nums)-1)
+// }
+
+// func helper(nums []int, index int, targetIndex int) bool {
+// 	if index <= 0 {
+// 		return nums[0] >= targetIndex
+// 	}
+
+// 	if index+nums[index] >= targetIndex {
+// 		return helper(nums, index-1, index)
+// 	}
+
+// 	return helper(nums, index-1, targetIndex)
+// }
+
+// func canJump(nums []int) bool {
+//     n = len(nums)
+//     steps := [n]bool
+
+//     nums[0] = true
+
+//     for i, _ := range(1, nums) {
+//         temp := false
+
+//         for j, _ := range(i-1, -1, -1) {
+//             if nums[j] > i - j && steps[j] == true {
+//                 steps[i] == true
+//                 break
+//             }
+//         }
+//     }
+//     return
+// }
+
+func canJump(nums []int) bool {
+	if len(nums) < 1 {
 		return true
 	}
+	memo := make(map[int]bool)
+	return jump(0, nums, memo)
 
-	if nums[0] > availSteps {
-		availSteps = nums[0]
+}
+
+func jump(pos int, nums []int, memo map[int]bool) bool {
+	if pos == len(nums)-1 {
+		return true
+	}
+	val, ok := memo[pos]
+
+	if ok {
+		return val
 	}
 
-	if availSteps == 0 {
+	if pos >= len(nums) || nums[pos] == 0 {
 		return false
 	}
 
-	return helper(nums[1:], availSteps-1)
+	for i := pos + nums[pos]; i >= pos+1; i-- {
+		if jump(i, nums, memo) {
+			memo[pos] = true
+			return true
+		}
+	}
+	memo[pos] = false
+
+	return false
+
 }
 
 func main() {
---
+++ triangle.go
@@ -0,0 +1,98 @@
+// Given a triangle array, return the minimum path sum from top to bottom.
+
+// For each step, you may move to an adjacent number of the row below.
+// More formally, if you are on index i on the current row, you may move to either
+// index i or index i + 1 on the next row.
+
+// Example 1:
+
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
+// Explanation: The triangle looks like:
+//   2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+// The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+// Example 2:
+
+// Input: triangle = [[-10]]
+// Output: -10
+
+// Constraints:
+
+// 1 <= triangle.length <= 200
+// triangle[0].length == 1
+// triangle[i].length == triangle[i - 1].length + 1
+// -104 <= triangle[i][j] <= 104
+
+// Follow up: Could you do this using only O(n) extra space, where n is the
+// total number of rows in the triangle?
+
+/*
+
+First attempt: doesn't use dp, works in some cases (starts at top and chooses lesser
+value until bottom). Doesn't work if the minimum sum is on a path with a higher value
+at any point.
+
+ */
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func minimumTotal(triangle [][]int) int {
+// 	sum := 0
+// 	return triangleHelper(triangle, sum, 0, 0)
+// }
+
+// func triangleHelper(triangle [][]int, sum int, index int, level int) int {
+// 	if level >= len(triangle) {
+// 		return sum
+// 	}
+
+// 	if level == 0 {
+// 		index = 0
+// 	} else if triangle[level][index] > triangle[level][index+1] {
+// 		fmt.Println(triangle[level][index], triangle[level][index+1])
+// 		index += 1
+// 	}
+
+// 	sum += triangle[level][index]
+
+// 	return triangleHelper(triangle, sum, index, level+1)
+// }
+
+func minimumTotal(triangle [][]int) int {
+	paths := make(map[[]int]int)
+	return triangleHelper(triangle, paths)
+}
+
+func triangleHelper(triangle, paths) {
+    // base case: when we get to bottom of triangle
+    
+    //    2
+    //   3 4
+    //  6 5 7
+    // 4 1 8 3
+    
+    // on the first level, there's only one option
+    // on the second level, there are two options: i == 0 or 1 [0][0] or [0][1]
+    // on the third level, we can go [0][0][0], [0][0][1], [0][1][1] or [0][1][2]
+    // on the fourth level [0][0][0][0], [0][0][0][1], [0][0][1][1], [0][0][1][2],
+    //                     [0][1][1][1], [0][1][1][2], [0][1][2][2], [0][1][2][3]
+    
+    // map:
+    // [0] :2
+    // [0, 0] : 5           add together the result of map[0] and triangle[1][0]
+    // [0, 1] : 6           add together the result of map[0] and triangle[1][1]
+    // [0, 0, 0] : 11       add together map[0, 0] and triangle[2][0]
+}
+
+
+func main() {
+	fmt.Println(minimumTotal([][]int{{1}, {2, 4}, {6, 5, 7}, {4, 1, 8, 3}}))
+	fmt.Println(minimumTotal([][]int{{-1}, {2, 3}, {1, -1, -3}}))
+}
